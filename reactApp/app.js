48b5f076bf5962b589e665fbe265dffa3d40d415
Xhonia Robinson <XDAWG@Xhonias-MacBook-Pro.local>
2017-08-07 13:42:27 -0700

    first commit

---
+++ reactApp/app.js
@@ -0,0 +1,21 @@
+var React = require('react');
+import ReactDOM from 'react-dom';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  render() {
+    return (
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
+  }
+}
+
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('root')
+);
