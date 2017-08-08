f3529460a6f43760e864bae5e5a9ab34565fc149
Xhonia Robinson <XDAWG@Xhonias-MacBook-Pro.local>
2017-08-08 11:45:40 -0700

    added forms and created editor file

--- reactApp/components/Login.js
+++ reactApp/components/Login.js
@@ -2,8 +2,47 @@ var React = require('react');
 
 const Login = ({})=>{
   return(
+    <div>
+    <div>
+      <div className={'has-text-centered'}><h1>Login</h1></div>
+
+
+  <div className={"field"}>
+  <label className={"label"}>Username</label>
+  <div className={"control has-icons-left has-icons-right"}>
+    <input className={"input is-success"} type="text" placeholder="Text input" value="bulma"/>
+    <span className={"icon is-small is-left"}>
+      <i className={"fa fa-user"}></i>
+    </span>
+    <span className={"icon is-small is-right"}>
+    </span>
+  </div>
+  </div>
+
+  <div className={"field"}>
+  <label className={"label"}>Password</label>
+  <div className={"control has-icons-left has-icons-right"}>
+    <input className={"input is-danger"} type="text" placeholder="Password" value=""/>
+    <span className={"icon is-small is-left"}>
+      <i className={"fa fa-envelope"}></i>
+    </span>
+    <span className={"icon is-small is-right"}>
+    </span>
+  </div>
+  </div>
+
+  </div>
+  <div className={"field is-grouped"}>
+  <div className={"control"}>
+    <button className={"button is-primary"}>Login</button>
+  </div>
+  </div>
+  <div className={"control"}>
+  <button className={"button "}>Register</button>
+  </div>
+    </div>
 
   )
 }
 
-export default 'Login';
+export default Login;
