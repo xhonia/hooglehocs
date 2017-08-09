9d6dbecb9e1d2e3a5c9454bb74c355c405c54307
git status <wangx@reed.edu>
2017-08-09 10:35:16 -0700

    page view fixed

--- reactApp/components/Register.js
+++ reactApp/components/Register.js
@@ -1,93 +1,96 @@
 var React = require('react');
 import ReactDOM from 'react-dom';
 import { HashRouter, Link, Redirect } from 'react-router-dom';
-
+var axios = require('axios');
 
 class Register extends React.Component{
   constructor(props) {
      super(props);
-
     this.state = {
      username: '',
-     password: ''
+     password: '',
+     rppassword:'',
+     email: '',
   };
 }
+
+verify(){
+  if(this.state.username===''){
+    alert('Username cannot be empty')
+  } else if(this.state.email===''){
+    alert('Email cannot be empty')
+  } else if(this.state.password===''){
+    alert('Password cannot be empty')
+  } else if(this.state.rppassword===''){
+    alert('Please confirm password')
+  } else if(this.state.password!==this.state.rppassword){
+    alert('Passwords do not match')
+  } else {
+    this.submit()
+  }
+}
+
+submit(){
+  axios.post('/register', {
+    username: this.state.username,
+    password: this.state.password,
+    email: this.state.email
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
 render(){
   return(
     <div>
       <div className={'has-text-centered'}><h1>Register</h1></div>
-      <div className={"field"}>
-        <label className={"label"}>Name</label>
-        <div className={"control"}>
-          <input className={"input"} type="text" placeholder="Text input"/>
-        </div>
-      </div>
+        <form className='container'>
+          <div className={"field"}>
+            <div className={"control has-icons-left has-icons-right"}>
+              <input className={"input is-danger"} type="text" onChange={(e)=>this.setState({username: e.target.value})} placeholder="Username" value={this.state.username}/>
+              <span className={"icon is-small is-left"}>
+                <i className={"fa fa-user"}></i>
+              </span>
+            </div>
+          </div>
+          <div className={"field"}>
+            <div className={"control has-icons-left has-icons-right"}>
+              <input className={"input is-danger"} type="text" onChange={(e)=>this.setState({email: e.target.value})} placeholder="Email" value={this.state.email}/>
+              <span className={"icon is-small is-left"}>
+                <i className={"fa fa-envelope"}></i>
+              </span>
+            </div>
+          </div>
+          <div className={"field"}>
+            <div className={"control has-icons-left has-icons-right"}>
+              <input className={"input is-danger"} type="password" onChange={(e)=>this.setState({password: e.target.value})} placeholder="Password" value={this.state.password}/>
+              <span className={"icon is-small is-left"}>
+                <i className={"fa fa-key"}></i>
+              </span>
+            </div>
+          </div>
+          <div className={"field"}>
+            <div className={"control has-icons-left has-icons-right"}>
+              <input className={"input is-danger"} type="password" onChange={(e)=>this.setState({rppassword: e.target.value})} placeholder="Confirm Password" value={this.state.rppassword}/>
+              <span className={"icon is-small is-left"}>
+                <i className={"fa fa-key"}></i>
+              </span>
+            </div>
+          </div>
+          <div className={"field is-grouped"}>
+            <div className={"control"}>
+              <button className={"button is-primary"} onClick={()=>this.verify()}>Submit</button>
+            </div>
+          </div>
 
-      <div className={"field"}>
-        <label className={"label"}>Username</label>
-        <div className={"control has-icons-left has-icons-right"}>
-          <input className={"input is-success"} type="text" placeholder="Text input" value="bulma"/>
-          <span className={"icon is-small is-left"}>
-            <i className={"fa fa-user"}></i>
-          </span>
-          <span className={"icon is-small is-right"}>
-            <i className={"fa fa-check"}></i>
-          </span>
-        </div>
-        <p className={"help is-success"}>This username is available</p>
-      </div>
+        </form>
 
-      <div className={"field"}>
-        <label className={"label"}>Email</label>
-        <div className={"control has-icons-left has-icons-right"}>
-          <input className={"input is-danger"} type="text" placeholder="Email input" value=""/>
-          <span className={"icon is-small is-left"}>
-            <i className={"fa fa-envelope"}></i>
-          </span>
-          <span className={"icon is-small is-right"}>
-            <i className={"fa fa-warning"}></i>
-          </span>
-        </div>
-        <p className={"help is-danger"}>This email is invalid</p>
-      </div>
-
-      <div className={"field"}>
-        <label className={"label"}>Password</label>
-        <div className={"control has-icons-left has-icons-right"}>
-          <input className={"input is-danger"} type="text" placeholder="Password" value=""/>
-          <span className={"icon is-small is-left"}>
-            <i className={"fa fa-envelope"}></i>
-          </span>
-          <span className={"icon is-small is-right"}>
-            <i className={"fa fa-warning"}></i>
-          </span>
-        </div>
-      </div>
-
-      <div className={"field"}>
-        <label className={"label"}>Confirm Password</label>
-        <div className={"control has-icons-left has-icons-right"}>
-          <input className={"input is-danger"} type="text" placeholder="Confirm Password" value=""/>
-          <span className={"icon is-small is-left"}>
-            <i className={"fa fa-envelope"}></i>
-          </span>
-          <span className={"icon is-small is-right"}>
-            <i className={"fa fa-warning"}></i>
-          </span>
-        </div>
-      </div>
-      <div className={"field is-grouped"}>
-        <div className={"control"}>
-          <button className={"button is-primary"}>Submit</button>
-        </div>
-        <div className={"control"}>
-          <button className={"button is-link"}>Cancel</button>
-        </div>
-      </div>
       <div className={"control"}>
-
         <button className={"button "} onClick={() => this.props.history.push('/')}>Login</button>
-
       </div>
     </div>
   )
